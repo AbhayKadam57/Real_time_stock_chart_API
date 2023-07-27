41c0199825ad9e21fb1329d139cfc32d0cbd4c92
abhay <kadamabhay40@gmail.com>
2023-07-27 12:14:32 +0530

    Fixed:cors issue in index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({ origin: ["*"] }));
 
 const server = app.listen(PORT, () => {
   console.log("Server is started at " + PORT);
@@ -22,7 +22,6 @@ const server = app.listen(PORT, () => {
 
 const io = socketio(server, {
   pingInterval: 25000, // Send a ping every 25 seconds
-  origin: ["*"],
 });
 
 io.on("connection", (socket) => {
